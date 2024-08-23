c4fdcf6c7ff5bf7cf10497fc1616f74caf11499b
Nour-MZ <77830568+Nour-MZ@users.noreply.github.com>
2024-08-23 14:50:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-$("#search-icon").addClass("show");
 
 $(".search-icon").on( "click", function() {
     $("#search-container").toggleClass("show");
     $("#search-modal").toggleClass("show")
-    console.log("goos")
   } );
 
-$(".search-close-btn").on( "click", function() {
-    $("#search-container").removeClass("show");
-    $("#search-modal").removeClass("show")
+$(".search-close-btn, #search-modal").on( "click", function() {
+    $(".search-icon").trigger("click")
 })
 
-$("#search-modal").on( "click", function() {
-    $("#search-container").removeClass("show");
-    $("#search-modal").removeClass("show")
-})
